094de80acdaf8bf5e55de79a1b15105abd7d2c01
Joshua Bloom <bloomblockchain@gmail.com>
2022-10-23 15:22:19 -0400

    Added comments.

--- community.py
+++ community.py
@@ -6,21 +6,42 @@
 from pyteal import *
 
 def approval_program():
+    
+    basic_checks = And(
+        Txn.rekey_to() == Global.zero_address(),    
+        Txn.close_remainder_to() == Global.zero_address(),
+        Txn.asset_close_to() == Global.zero_address()
+    )
+
+    # creates the smart contract and the values within it
     on_creation = Seq(
         [
+            ## WHY? ## checks that the application argument length is one
             Assert(Txn.application_args.length() == Int(1)),
-            App.globalPut(Bytes("total supply"), Btoi(Txn.application_args[0])),
-            App.globalPut(Bytes("reserve"), Btoi(Txn.application_args[0])),
+            # total amount
+            App.globalPut(Bytes("total amount"), Btoi(Txn.application_args[0])), # 1000000
+            # decimals
+            App.globalPut(Bytes("decimals"), Btoi(Txn.application_args[1])), # 0
+            # asset name
+            App.globalPut(Bytes("asset name"), Txn.application_args[2]), # Tesla
+            # asset unit name
+            App.globalPut(Bytes("asset unit name"), Txn.application_args[3]), # TSLA
+            # local put that designates the minter as the admin of the asset
             App.localPut(Int(0), Bytes("admin"), Int(1)),
+            # local put that tracks the balance of the asset
             App.localPut(Int(0), Bytes("balance"), Int(0)),
-            Return(Int(1)),
+            # approves the sequence by leaving one on the stack
+            Return(Int(1))
         ]
     )
 
+    # returns boolean showing if address is admin
     is_admin = App.localGet(Int(0), Bytes("admin"))
 
+    # allows accounts to opt out of the application
     on_closeout = Seq(
         [
+            # returns the balance of the account to the reserve
             App.globalPut(
                 Bytes("reserve"),
                 App.globalGet(Bytes("reserve"))
@@ -30,81 +51,99 @@ def approval_program():
         ]
     )
 
-    register = Seq([App.localPut(Int(0), Bytes("balance"), Int(0)), Return(Int(1))])
+    # opts an acct into the app
+    register = Seq(
+        [
+            App.localPut(
+                Int(0), 
+                Bytes("balance"), 
+                Int(0)),
+            Return(Int(1))
+        ]
+    )
 
-    # configure the admin status of the account Txn.accounts[1]
+    # set admin to Txn.accounts[1]
     # sender must be admin
     new_admin_status = Btoi(Txn.application_args[1])
     set_admin = Seq(
         [
             Assert(And(is_admin, Txn.application_args.length() == Int(2))),
+            # sets the first account to the new admin
             App.localPut(Int(1), Bytes("admin"), new_admin_status),
-            Return(Int(1)),
+            Return(Int(1))
         ]
     )
-    # NOTE: The above set_admin code is carefully constructed. If instead we used the following code:
-    # Seq([
-    #     Assert(Txn.application_args.length() == Int(2)),
-    #     App.localPut(Int(1), Bytes("admin"), new_admin_status),
-    #     Return(is_admin)
-    # ])
-    # It would be vulnerable to the following attack: a sender passes in their own address as
-    # Txn.accounts[1], so then the line App.localPut(Int(1), Bytes("admin"), new_admin_status)
-    # changes the sender's admin status, meaning the final Return(is_admin) can return anything the
-    # sender wants. This allows anyone to become an admin!
 
-    # move assets from the reserve to Txn.accounts[1]
-    # sender must be admin
+    # separating the mint amount as a var, first app arg is amount
     mint_amount = Btoi(Txn.application_args[1])
+    # subroutine mints assets
     mint = Seq(
         [
+            # must be two application arguments
             Assert(Txn.application_args.length() == Int(2)),
+            # cannot mint more than the acct balance of the minter
             Assert(mint_amount <= App.globalGet(Bytes("reserve"))),
+            # updates the global reserve in the sc
             App.globalPut(
-                Bytes("reserve"), App.globalGet(Bytes("reserve")) - mint_amount
+                Bytes("reserve"),
+                App.globalGet(Bytes("reserve")) - mint_amount,
             ),
+            # updates the local balance
             App.localPut(
-                Int(1),
+                Int(1), # does the order of the array change based on who the sender of the transaction is?
                 Bytes("balance"),
-                App.localGet(Int(1), Bytes("balance")) + mint_amount,
+                App.localGet(Int(0), Bytes("balance")) + mint_amount,
             ),
-            Return(is_admin),
         ]
     )
 
-    # transfer assets from the sender to Txn.accounts[1]
+    # set amount to transfer as app arg one
     transfer_amount = Btoi(Txn.application_args[1])
+    # transfers the asset
     transfer = Seq(
         [
+            # requires there are two app args
             Assert(Txn.application_args.length() == Int(2)),
+            # txn amount is less than the local acct balance
             Assert(transfer_amount <= App.localGet(Int(0), Bytes("balance"))),
+            # changing balance for sending acct
             App.localPut(
                 Int(0),
                 Bytes("balance"),
                 App.localGet(Int(0), Bytes("balance")) - transfer_amount,
             ),
+            # changing balance for receiving acct
             App.localPut(
                 Int(1),
                 Bytes("balance"),
                 App.localGet(Int(1), Bytes("balance")) + transfer_amount,
             ),
-            Return(Int(1)),
+            Return(Int(1))
         ]
     )
 
     program = Cond(
+        # checks if the smart contract already exists
         [Txn.application_id() == Int(0), on_creation],
+        # deletes the application
         [Txn.on_completion() == OnComplete.DeleteApplication, Return(is_admin)],
+        # update application subroutine
         [Txn.on_completion() == OnComplete.UpdateApplication, Return(is_admin)],
+        # closeout application subroutine
         [Txn.on_completion() == OnComplete.CloseOut, on_closeout],
+        # opt in subroutine
         [Txn.on_completion() == OnComplete.OptIn, register],
+        # sets the administrator 
         [Txn.application_args[0] == Bytes("set admin"), set_admin],
+        # mints the asset
         [Txn.application_args[0] == Bytes("mint"), mint],
+        # transfers the asset
         [Txn.application_args[0] == Bytes("transfer"), transfer],
+        # burns the asset
+        [Txn.application_args[0] == Bytes("burn"), burn]
     )
 
     return program
-
 
 def clear_state_program():
     program = Seq(
