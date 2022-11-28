f49183651315a54f12a0e2cc8f5a7b0ff4fade86
Joshua Bloom <96476058+AlgoBloom@users.noreply.github.com>
2022-11-27 19:21:09 -0500

    Added variables class under a new item class. Filled in variables we plan to use for the items in the marketplace.

--- marketplace.py
+++ marketplace.py
@@ -2,3 +2,12 @@
 # buy product by making a call to the app and performing a payment to the creator
 
 from pyteal import *
+
+class Item:
+    class Vars:
+        supplier = Bytes("Supplier")
+        name = Bytes("Name")
+        image = Bytes("Image")
+        description = Bytes("Description")
+        price = Bytes("Price")
+        sold = Bytes("Sold")
